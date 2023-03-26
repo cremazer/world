fc9dad6c5c2e2a18e37d916f5a14782c6fd4bb5f
cremazer <cremazer@gmail.com>
2023-03-27 07:26:15 +0900

    [BAC-17] Develop a member search function. - add memberList.html - add search member function

--- src/main/java/hello/world/controller/MemberController.java
+++ src/main/java/hello/world/controller/MemberController.java
@@ -3,8 +3,10 @@ package hello.world.controller;
 import hello.world.domain.Member;
 import hello.world.model.member.MemberRequestForm;
 import hello.world.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -25,9 +27,16 @@ public class MemberController {
     @PostMapping("/member/new")
     public String createNewMember(MemberRequestForm form) {
         Member member = new Member();
-        member.setName(createForm());
+        member.setName(form.getName());
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "member/memberList";
     }
 }
