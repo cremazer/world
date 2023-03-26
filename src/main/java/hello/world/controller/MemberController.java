a3021bcccf4a7d53d91cfb92944f1000fa3f2452
cremazer <cremazer@gmail.com>
2023-03-26 21:00:02 +0900

    [BAC-16] Develop membership registration function. - add createMemberForm.html - add createNewMember function

--- src/main/java/hello/world/controller/MemberController.java
+++ src/main/java/hello/world/controller/MemberController.java
@@ -1,8 +1,12 @@
 package hello.world.controller;
 
+import hello.world.domain.Member;
+import hello.world.model.member.MemberRequestForm;
 import hello.world.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +15,19 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/member/new")
+    public String createForm() {
+        return "member/createMemberForm";
+    }
+
+    @PostMapping("/member/new")
+    public String createNewMember(MemberRequestForm form) {
+        Member member = new Member();
+        member.setName(createForm());
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
